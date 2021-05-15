0149eab60073006a33f0450c84ed1bccc548ec01
”Ivan <Trech06@mail.ru>
2021-05-15 21:32:16 +0300

    Program finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        System.out.println(inputString);
+        int intList = 0;
+        char[] charList = inputString.toCharArray();
+        for (char c:charList
+             ) {
+            intList += Integer.parseInt(Character.toString(c));
+        }
+        System.out.println(intList);
     }
 }
